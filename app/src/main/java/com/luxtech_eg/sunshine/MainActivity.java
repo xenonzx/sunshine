091bb5b3c96cd238aab87610d4ff5ae9813f063f
Ahmed El-Dakhakhny <ahmed0xenon@gmail.com>
2015-11-24 15:43:31 +0200

    list view with temp data

--- app/src/main/java/com/luxtech_eg/sunshine/MainActivity.java
+++ app/src/main/java/com/luxtech_eg/sunshine/MainActivity.java
@@ -7,6 +7,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -32,6 +36,15 @@ public class MainActivity extends AppCompatActivity {
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
             View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            ListView lv= (ListView)rootView.findViewById(R.id.listview_forcast);
+            ArrayList<String> tempInfo= new ArrayList<String>();
+            tempInfo.add("today-23");
+            tempInfo.add("sun-23");
+            tempInfo.add("mon-23");
+            tempInfo.add("tuey-23");
+
+            ArrayAdapter<String>  mArrayAdapter= new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textview,tempInfo);
+            lv.setAdapter(mArrayAdapter);
             return rootView;
         }
     }
