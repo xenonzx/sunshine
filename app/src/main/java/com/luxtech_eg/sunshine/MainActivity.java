aa4fcf521511ce21f88410f6156221708500a119
Ahmed El-Dakhakhny <ahmed0xenon@gmail.com>
2015-11-24 15:27:14 +0200

    frame layout and only one listview as achild

--- app/src/main/java/com/luxtech_eg/sunshine/MainActivity.java
+++ app/src/main/java/com/luxtech_eg/sunshine/MainActivity.java
@@ -3,11 +3,8 @@ package com.luxtech_eg.sunshine;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
