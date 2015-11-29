e33ceb84298018f7bff54074e81073b66f0b111e
Ahmed El-Dakhakhny <ahmed0xenon@gmail.com>
2015-11-29 14:37:35 +0200

    lunching DetailActivity byclicking on list item

--- app/src/main/java/com/luxtech_eg/sunshine/ForecastFragment.java
+++ app/src/main/java/com/luxtech_eg/sunshine/ForecastFragment.java
@@ -1,5 +1,6 @@
 package com.luxtech_eg.sunshine;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -78,6 +80,14 @@ public  class ForecastFragment extends Fragment {
 
         mArrayAdapter= new ArrayAdapter<String>(getActivity(),R.layout.list_item_forecast,R.id.list_item_forecast_textview,tempInfo);
         lv.setAdapter(mArrayAdapter);
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.v(TAG,mArrayAdapter.getItem(position));
+                Intent i = new Intent (getActivity(),DetailActivity.class);
+                startActivity(i);
+            }
+        });
 
         return rootView;
     }
