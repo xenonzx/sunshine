7669c652fc901cc43abd4a032cca77c046fcc587
Ahmed El-Dakhakhny <ahmed0xenon@gmail.com>
2015-12-17 14:00:05 +0200

    lesson 4c

--- app/src/main/java/com/luxtech_eg/sunshine/DetailActivity.java
+++ app/src/main/java/com/luxtech_eg/sunshine/DetailActivity.java
@@ -1,8 +1,12 @@
 package com.luxtech_eg.sunshine;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.ShareActionProvider;
@@ -15,7 +19,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.luxtech_eg.sunshine.data.WeatherContract;
+import com.luxtech_eg.sunshine.data.WeatherContract.WeatherEntry;
+
 public class DetailActivity extends AppCompatActivity {
+    private static final int LOADER_ID = 1;
     private ShareActionProvider mShareActionProvider;
 
     @Override
@@ -57,13 +65,29 @@ public class DetailActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+
+
     /**
      * A placeholder fragment containing a simple view.
      */
-    public static class PlaceholderFragment extends Fragment {
+    public static class PlaceholderFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
         String TAG=PlaceholderFragment.class.getSimpleName();
         TextView forecastTV;
         String forecast;
+        ShareActionProvider mShareActionProvider;
+        private static final String[] FORECAST_COLUMNS = {
+                WeatherEntry.TABLE_NAME + "." + WeatherEntry._ID,
+                WeatherContract.WeatherEntry.COLUMN_DATE,
+                WeatherEntry.COLUMN_SHORT_DESC,
+                WeatherEntry.COLUMN_MAX_TEMP,
+                WeatherContract.WeatherEntry.COLUMN_MIN_TEMP,
+        };
+        private static final int COL_WEATHER_ID = 0;
+        private static final int COL_WEATHER_DATE = 1;
+        private static final int COL_WEATHER_DESC = 2;
+        private static final int COL_WEATHER_MAX_TEMP = 3;
+        private static final int COL_WEATHER_MIN_TEMP = 4;
+
         public PlaceholderFragment() {
             setHasOptionsMenu(true);
         }
@@ -75,17 +99,17 @@ public class DetailActivity extends AppCompatActivity {
             View rootView = inflater.inflate(R.layout.fragment_detail, container, false);
             forecastTV=(TextView) rootView.findViewById(R.id.tv_forcast);
 
-            Intent intent = getActivity().getIntent();
-            if (intent != null) {
-                forecast = intent.getDataString();
-            }
 
-            //String forecast = getActivity().getIntent().getStringExtra("forecastString");
-            if(null!= forecast) {
-                forecastTV.setText(forecast);
-            }
             return rootView;
         }
+
+        @Override
+        public void onActivityCreated(Bundle savedInstanceState) {
+            // register rh loader with loader manager
+            getLoaderManager().initLoader(LOADER_ID, null, this);
+            super.onActivityCreated(savedInstanceState);
+        }
+
         Intent createShareIntent(){
             Intent mShareIntent = new Intent();
             mShareIntent.setAction(Intent.ACTION_SEND);
@@ -102,8 +126,9 @@ public class DetailActivity extends AppCompatActivity {
             MenuItem item = menu.findItem(R.id.menu_item_share);
 
             // for compatibility  MenuItemCompat
-            ShareActionProvider mShareActionProvider =(ShareActionProvider) MenuItemCompat.getActionProvider(item);
-            if (mShareActionProvider!=null){
+            mShareActionProvider =(ShareActionProvider) MenuItemCompat.getActionProvider(item);
+            if (forecast!=null){
+                // if we already have a forecast
                 mShareActionProvider.setShareIntent(createShareIntent());
             }
             else{
@@ -111,5 +136,63 @@ public class DetailActivity extends AppCompatActivity {
             }
 
         }
-    }
+
+        @Override
+        public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+            //make the query
+            Intent intent = getActivity().getIntent();
+            if (intent == null) {
+                return null;
+            }
+
+            // Now create and return a CursorLoader that will take care of
+            // creating a Cursor for the data being displayed.
+            return new CursorLoader(
+                    getActivity(),
+                    intent.getData(),
+                    FORECAST_COLUMNS,
+                    null,
+                    null,
+                    null
+            );
+
+        }
+
+        @Override
+        public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+            // can modify ui
+            Log.v(TAG, "In onLoadFinished");
+            if (!data.moveToFirst()) { return; }
+
+            String dateString = Utility.formatDate(
+                    data.getLong(COL_WEATHER_DATE));
+
+            String weatherDescription =
+                    data.getString(COL_WEATHER_DESC);
+
+            boolean isMetric = Utility.isMetric(getActivity());
+
+            String high = Utility.formatTemperature(
+                    data.getDouble(COL_WEATHER_MAX_TEMP), isMetric);
+
+            String low = Utility.formatTemperature(
+                    data.getDouble(COL_WEATHER_MIN_TEMP), isMetric);
+
+            forecast = String.format("%s - %s - %s/%s", dateString, weatherDescription, high, low);
+            TextView detailTextView = (TextView)getView().findViewById(R.id.tv_forcast);
+            detailTextView.setText(forecast);
+
+            // If onCreateOptionsMenu has already happened, we need to update the share intent now.
+            // avoiding thread happened before main thread
+            if (mShareActionProvider != null) {
+                mShareActionProvider.setShareIntent(createShareIntent());
+            }
+        }
+
+        @Override
+        public void onLoaderReset(Loader<Cursor> loader) {
+            // no data to be cleared
+        }
+
+         }
 }
