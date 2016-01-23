0dc0bb5310cc316b5c5421fd995987d3181db52d
Ahmed El-Dakhakhny <ahmed0xenon@gmail.com>
2016-01-23 11:30:35 +0200

    notification

--- app/src/main/java/com/luxtech_eg/sunshine/Utility.java
+++ app/src/main/java/com/luxtech_eg/sunshine/Utility.java
@@ -29,7 +29,7 @@ public class Utility {
                 .equals(context.getString(R.string.pref_temp_setting_celsius));
     }
 
-    static String formatTemperature(Context context,double temperature, boolean isMetric) {
+    public static String formatTemperature(Context context,double temperature, boolean isMetric) {
         double temp;
         if (!isMetric) {
             temp = 9 * temperature / 5 + 32;
